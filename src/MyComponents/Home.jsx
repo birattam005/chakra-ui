500ae8e926d22cf101283281adfc687a1ddaad9f
Birattam Rajak <birattam444@gmail.com>
2023-05-01 20:39:20 +0530

    added responsive container

--- src/MyComponents/Home.jsx
+++ src/MyComponents/Home.jsx
@@ -6,7 +6,7 @@ import img1 from '../assets/1.jpg';
 import img2 from '../assets/2.jpg';
 import img3 from '../assets/3.jpg';
 import img4 from '../assets/4.jpg';
-import img5 from '../assets/5.jpg';
+import img5 from '../assets/5.png';
 
 const headingOptions = {
   pos: 'absolute',
@@ -23,6 +23,27 @@ const Home = () => {
     <Box>
 
         <Mycarousel />
+        <Container maxW={'container.xl'} minH={'100vh'} p={'16'}>
+            <Heading 
+            textTransform={'uppercase'} py={'2'}
+            w={'fit-content'} borderBottom={'2px solid'} m={'auto'}>
+            Services</Heading>
+
+            <Stack 
+             h={'full'}
+             p={'4'}
+             alignItems={'center'}
+             direction={['column','row']}>
+                <Image src= {img5} h={['40','400']} filter={'hue-rotate(-130deg)'} />
+
+              <Text letterSpacing={'widest'} lineHeight={'190%'} p={['4','16']}>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere ex eaque pariatur laborum aliquam, quaerat quae in maiores voluptates, reiciendis praesentium minima veniam ullam nemo vel similique nobis voluptate itaque alias, odit consequatur amet dolor dolores. Fugit recusandae pariatur et repellat sed aliquam earum, dolor deleniti perspiciatis dolorum cum est consectetur distinctio quis. Earum quod doloremque natus explicabo saepe sequi fugiat dignissimos id quos ducimus autem provident, blanditiis officiis facere dolor, nam ex excepturi dicta. Ipsam quam exercitationem officia accusantium voluptatem soluta porro recusandae asperiores quas quidem molestias pariatur, praesentium doloremque dolore qui deserunt, dolorum quod veritatis rerum assumenda harum.
+              </Text>
+
+            </Stack>
+        
+        </Container>
+
 
     </Box>
   );
