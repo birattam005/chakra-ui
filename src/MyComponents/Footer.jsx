90a52a3529bf79e4e41e17f84c63d12e8aa3555a
Birattam Rajak <birattam444@gmail.com>
2023-05-01 22:31:43 +0530

    footer complete

--- src/MyComponents/Footer.jsx
+++ src/MyComponents/Footer.jsx
@@ -1,4 +1,4 @@
-import { Box,Button,HStack,Heading, Input, Stack, VStack } from '@chakra-ui/react'
+import { Box,Button,HStack,Heading, Input, Stack, VStack,Text } from '@chakra-ui/react'
 import React from 'react'
 import {AiOutlineSend} from 'react-icons/ai'
 
@@ -7,8 +7,8 @@ const Footer = () => {
     <Box 
     bgColor={'blackAlpha.900'} minH={'40'} p={'16'} color={'white'}>
         <Stack direction={['column','row']}>
-            <VStack>
-                <Heading size={'md'} textTransform={'uppercase'}> Subscribe to my channel
+            <VStack alignItems={'stretch'} w={'full'} px={'4'}>
+                <Heading size={'md'} textTransform={'uppercase'} textAlign={['center','left']}> Subscribe to my channel
                 </Heading>
                 <HStack 
                     borderBottom={'2px solid white'}
@@ -28,7 +28,30 @@ const Footer = () => {
                 </HStack>
             </VStack>
 
-            
+                <VStack 
+                w={'full'}
+                borderLeft={['none','1px solid white']}
+                borderRight={['none','1px solid white']}>
+                
+                    <Heading 
+                        textTransform={'uppercase'}
+                        textAlign={'center'}
+                    >
+                        VIDEO HUB   
+                    </Heading>
+                     <Text> All rights received</Text>
+                </VStack>
+
+                <VStack 
+                w={'full'}>
+                <Heading size={'md'} textTransform={'uppercase'}>Social Media</Heading>
+                <Button variant={'link'} colorScheme='whiteAlpha' >
+                        <a href="https://twitter.com/Biru32624195"> Twitter</a>
+                </Button>
+                <Button variant={'link'} colorScheme='whiteAlpha' >
+                        <a href="https://www.linkedin.com/in/birattam005/"> Linkdin</a>
+                </Button>
+                </VStack>
 
         </Stack>
 
