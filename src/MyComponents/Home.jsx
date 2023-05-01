22ad0bea8a68bdf1418d8118ea69e9c3550785ca
Birattam Rajak <birattam444@gmail.com>
2023-05-01 16:55:44 +0530

    carousel created

---
+++ src/MyComponents/Home.jsx
@@ -0,0 +1,56 @@
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.jpg';
+
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%,-50%)',
+  textTransform: 'uppercase',
+  p: '4',
+  size: '4xl',
+};
+
+const Home = () => {
+  return (
+    <Box>
+
+        <Mycarousel />
+
+    </Box>
+  );
+};
+
+const Mycarousel = () => (
+      <Carousel autoPlay infiniteLoop interval={1000} showStatus= {false} showThumbs={false}>
+
+      <Box w="full" h={'100vh'}>
+          <Image src= {img1} />
+          <Heading bgColor={"blackAlpha.600"} color={"white"} {...headingOptions}> Wactch the Future</Heading>
+      </Box>
+
+      <Box w="full" h={'100vh'}>
+          <Image src= {img3} />
+          <Heading bgColor={"whiteAlpha.600"} color={"black"} {...headingOptions}>Gaming on Console</Heading>
+      </Box>
+
+      <Box w="full" h={'100vh'}>
+          <Image src= {img4} />
+          <Heading bgColor={"whiteAlpha.600"} color={"black"} {...headingOptions}>Night Life is cool</Heading>
+      </Box>
+
+      <Box w="full" h={'100vh'}>
+          <Image src= {img2} />
+          <Heading bgColor={"whiteAlpha.600"} color={"black"} {...headingOptions}>Future is Gaming</Heading>
+      </Box>
+
+      </Carousel>
+)
+export default Home;
